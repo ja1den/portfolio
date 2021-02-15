624d95fd7430aa9989c4ce3c4b969009237d7af6
Jaiden Douglas <49811314+ja1den@users.noreply.github.com>
2021-02-15 11:52:45 +1030

    feat: add about command

--- src/lib/commands.tsx
+++ src/lib/commands.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 export type Command = [
 	{
@@ -23,12 +23,38 @@ const commands: Command[] = [
 		() => {
 			const length = Math.max(...commands.map(command => command[0].usage.length)) + 2;
 
-			const message = '\n' + 
+			const message = '\nUsage: <command> [args]\n\n' + 
 				commands.map(command => command[0].usage + ' '.repeat(length - command[0].usage.length) + command[0].description)
 					.sort().join('\n');
 			
 			return <p>{message}</p>;
 		}
+	],
+	[
+		{
+			name: 'about',
+			description: 'learn about me',
+			usage: 'about'
+		},
+		() => (
+			<Fragment>
+				<br />
+				<span>Hey, I'm Jaiden Douglas!</span>
+				<br />
+				<br />
+				<span>
+					I'm an Australian full stack developer, working primarily in{' '}
+					<a href='https://www.typescriptlang.org/'>TypeScript</a>.
+				</span>
+				<br />
+				<br />
+				<span>
+					I like <a href='https://reactjs.org/'>React</a>,{' '}
+					<a href='https://nodejs.org/en/'>Node.js</a>, and{' '}
+					<a href='https://www.rust-lang.org/'>Rust</a>.
+				</span>
+			</Fragment>
+		)
 	]
 ];
 
